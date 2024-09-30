8523bb83ddc0a817e8f970677f20a217e858fb41
rimasultana <rimasultana20225@gmail.com>
2024-09-30 18:16:02 +0600

    callback

--- prectic/index.js
+++ prectic/index.js
@@ -1,11 +1,53 @@
-function myDisplayer(some) {
+/* function myDisplayer(some) {
    console.log(some);
   }
   
   function myCalculator(num1, num2) {
     let sum = num1 + num2;
-    return sum;
+   myDisplayer(sum);
   }
   
-  let result = myCalculator(5, 5);
-  myDisplayer(result);
+  myCalculator(5, 5);
+   */
+
+  function display (some){
+    console.log(some);
+  }
+
+  function calculator(num1 , num2, callback){
+    let sum = num1 + num2;
+    callback (sum);
+
+  }
+
+  // calculator(5, 5, display);
+
+
+  const pymentSuccess = true;
+  const mark = 70;
+
+
+
+  function enroll(callback){
+    console.log("Course is enroll is in progress");;
+
+    setTimeout(function(){
+      if(pymentSuccess){
+        callback();
+      }else{
+        console.log("payment is failed");
+      }
+    }, 2000);
+  }
+
+  function progress(callback){
+    console.log("Course on progress");
+
+    setTimeout(function(){
+      if(mark >= 80){
+        callback();
+      }else{
+        console.log("you could not get enough");
+      }
+    }, 3000);
+  }
