5b638b5a9833eb7cde4536a55b7202cec733910e
rimasultana <rimasultana20225@gmail.com>
2024-10-02 23:01:04 +0600

    json

---
+++ Module-33/js/comment.js
@@ -0,0 +1,16 @@
+function lodeData() {
+  fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("error happened", error));
+}
+
+async function lodeData2() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log("data load error", error);
+  }
+}
--- Module-33/post.js
+++ Module-33/post.js
@@ -1,21 +1,20 @@
 function loadData() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => displayPost(data));
-  }
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayPost(data));
+}
 
-  function displayPost(posts) {
-    const postContainer = document.getElementById("posts-container");
+function displayPost(posts) {
+  const postContainer = document.getElementById("posts-container");
 
-    for (const post of posts) {
-      const div = document.createElement("div");
-      div.classList.add('post')
-      div.innerHTML = `
-      
-        <h4>User:${post.userId}</h4>
+  for (const post of posts) {
+    const div = document.createElement("div");
+    div.classList.add("post");
+    div.innerHTML = `
+      <h4>User:${post.userId}</h4>
         <h4>Post: ${post.title}</h4>
         <p>Post Description: ${post.body}</p>
       `;
-      postContainer.appendChild(div);
-    }
+    postContainer.appendChild(div);
   }
+}
