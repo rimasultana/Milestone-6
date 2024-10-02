7bc7155b246728760e7f7622d0d2df3a27d21476
rimasultana <rimasultana20225@gmail.com>
2024-10-02 19:24:33 +0600

    json

---
+++ Module-33/post.js
@@ -0,0 +1,21 @@
+function loadData() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => displayPost(data));
+  }
+
+  function displayPost(posts) {
+    const postContainer = document.getElementById("posts-container");
+
+    for (const post of posts) {
+      const div = document.createElement("div");
+      div.classList.add('post')
+      div.innerHTML = `
+      
+        <h4>User:${post.userId}</h4>
+        <h4>Post: ${post.title}</h4>
+        <p>Post Description: ${post.body}</p>
+      `;
+      postContainer.appendChild(div);
+    }
+  }
